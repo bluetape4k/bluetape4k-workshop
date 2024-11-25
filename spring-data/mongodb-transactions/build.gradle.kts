dd33d50cf6c4c22c6459cefba9e796852572037f
Debop <sunghyouk.bae@gmail.com>
2024-11-25 22:53:13 +0900

    mongodb transaction 예제
    Signed-off-by: Debop <sunghyouk.bae@gmail.com>

---
+++ spring-data/mongodb-transactions/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    kotlin("plugin.spring")
+}
+
+@Suppress("UnstableApiUsage")
+configurations {
+    testImplementation.get().extendsFrom(compileOnly.get(), runtimeOnly.get())
+}
+
+dependencies {
+    implementation(Libs.springBootStarter("actuator"))
+    implementation(Libs.springBootStarter("aop"))
+    implementation(Libs.springBootStarter("webflux"))
+    implementation(Libs.springBootStarter("validation"))
+
+    implementation(Libs.springBootStarter("data-mongodb"))
+    implementation(Libs.springBootStarter("data-mongodb-reactive"))
+
+    runtimeOnly(Libs.springBoot("devtools"))
+    annotationProcessor(Libs.springBoot("configuration-processor"))
+
+    testImplementation(Libs.bluetape4k_spring_tests)
+    testImplementation(Libs.springBootStarter("test")) {
+        exclude(group = "junit", module = "junit")
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+        exclude(module = "mockito-core")
+    }
+
+    // Mongo Driver
+    implementation(Libs.mongodb_driver_sync)
+    implementation(Libs.mongodb_driver_reactivestreams)
+
+    // MongoDB Testcontainers
+    implementation(Libs.bluetape4k_testcontainers)
+    implementation(Libs.testcontainers)
+    implementation(Libs.testcontainers_mongodb)
+
+    // Coroutines
+    implementation(Libs.bluetape4k_coroutines)
+    implementation(Libs.kotlinx_coroutines_core)
+    implementation(Libs.kotlinx_coroutines_reactor)
+    testImplementation(Libs.kotlinx_coroutines_test)
+
+    implementation(Libs.bluetape4k_jackson)
+    implementation(Libs.bluetape4k_idgenerators)
+    testImplementation(Libs.bluetape4k_junit5)
+
+    testImplementation(Libs.reactor_test)
+    testImplementation(Libs.turbine)
+}
