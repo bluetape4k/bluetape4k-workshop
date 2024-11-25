73e90b3bf97c1a8305ba72f79bf18fbee12c8256
Debop <sunghyouk.bae@gmail.com>
2024-11-25 20:52:51 +0900

    webflux + redis cache 예제 추가
    Signed-off-by: Debop <sunghyouk.bae@gmail.com>

---
+++ spring-boot/cache-redis/src/main/kotlin/io/bluetape4k/workshop/cache/redis/RedisCacheApplication.kt
@@ -0,0 +1,22 @@
+package io.bluetape4k.workshop.cache.redis
+
+import io.bluetape4k.testcontainers.storage.RedisServer
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@SpringBootApplication
+@EnableScheduling
+class RedisCacheApplication {
+
+    companion object {
+        @JvmStatic
+        val redisServer = RedisServer.Launcher.redis
+    }
+}
+
+fun main(vararg args: String) {
+    runApplication<RedisCacheApplication>(*args) {
+        setAdditionalProfiles("app")
+    }
+}
