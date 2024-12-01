8f5ea6bc73dde9e1307f6b3b6498f0513f9fae86
Debop <sunghyouk.bae@gmail.com>
2024-12-01 11:20:27 +0900

    Bucket4j 를 이용하여 User Id 기준으로 Rate limit 을 적용하는 예제
    Signed-off-by: Debop <sunghyouk.bae@gmail.com>

---
+++ ratelimit/README.md
@@ -0,0 +1,17 @@
+# Rate Limiter 예제
+
+## bucket4j-webflux-demo (추천)
+
+`kommons-bucket4j` 에서 제공하는 RateLimiter 를 사용한 예제입니다.
+
+IP 기반, 사용자 토큰 기반 모두 제공하고, Redis 를 Bucket 저장소로 사용합니다.
+
+## bucket4j-redis-demo
+
+`bucket4j-spring-boot-starter` 를 사용한 예제입니다.
+
+IP 기반 Rate Limiter를 제공하고, 로컬용입니다.
+
+## bucket4j-user-demo
+
+사용자 토큰 기반의 Rate Limiter를 제공합니다.
---
+++ ratelimit/bucker4j-user-webflux/src/main/kotlin/io/bluetape4k/workshop/bucket4j/controller/ReactiveController.kt
@@ -0,0 +1,30 @@
+package io.bluetape4k.workshop.bucket4j.controller
+
+import io.bluetape4k.logging.KLogging
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import java.time.Instant
+
+@RestController
+class ReactiveController {
+
+    companion object: KLogging()
+
+    /**
+     * `/api/v1/reactive/~~~` 는 Rate limit를 적용한다
+     */
+    @GetMapping("/api/v1/reactive/hello")
+    fun helloV1(): Mono<String> {
+        return Mono.just("Hello World V1 at " + Instant.now().toString())
+    }
+
+    /**
+     * `/api/v2/reactive/~~~` 는 Rate limit를 걸지 않는다 (Bucket4j 적용 안함)
+     *
+     */
+    @GetMapping("/api/v2/reactive/hello")
+    fun helloV2(): Mono<String> {
+        return Mono.just("Hello World V2 at " + Instant.now().toString())
+    }
+}
