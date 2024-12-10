17ad33bd8817829617c60a632c0454d09a0851f8
Debop <sunghyouk.bae@gmail.com>
2024-12-10 14:51:50 +0900

    chore: Add logging message

--- spring-security/mvc/hello/src/main/kotlin/io/bluetape4k/workshop/spring/security/mvc/KotlinApplication.kt
+++ spring-security/mvc/hello/src/main/kotlin/io/bluetape4k/workshop/spring/security/mvc/KotlinApplication.kt
@@ -1,12 +1,17 @@
 package io.bluetape4k.workshop.spring.security.mvc
 
 import io.bluetape4k.logging.KLogging
+import io.bluetape4k.logging.info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class KotlinApplication {
-    companion object: KLogging()
+    companion object: KLogging() {
+        init {
+            log.info { "Starting KotlinApplication" }
+        }
+    }
 }
 
 fun main(vararg args: String) {
--- spring-security/webflux/hello-security/src/main/kotlin/io/bluetape4k/workshop/spring/security/webflux/KotlinWebfluxApplication.kt
+++ spring-security/webflux/hello-security/src/main/kotlin/io/bluetape4k/workshop/spring/security/webflux/KotlinWebfluxApplication.kt
@@ -1,13 +1,18 @@
 package io.bluetape4k.workshop.spring.security.webflux
 
 import io.bluetape4k.logging.KLogging
+import io.bluetape4k.logging.debug
 import org.springframework.boot.WebApplicationType.REACTIVE
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class KotlinWebfluxApplication {
-    companion object: KLogging()
+    companion object: KLogging() {
+        init {
+            log.debug { "Starting KotlinWebfluxApplication" }
+        }
+    }
 }
 
 fun main(vararg args: String) {
