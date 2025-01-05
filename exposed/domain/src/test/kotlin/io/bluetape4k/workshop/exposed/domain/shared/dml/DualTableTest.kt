018ac14ce840ab2f677b0c6af7cc29608ed24377
Debop <sunghyouk.bae@gmail.com>
2025-01-05 18:43:57 +0900

    chore: Dual Table 활용 예

---
+++ exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/shared/dml/DualTableTest.kt
@@ -0,0 +1,31 @@
+package io.bluetape4k.workshop.exposed.domain.shared.dml
+
+import io.bluetape4k.workshop.exposed.domain.AbstractExposedTest
+import io.bluetape4k.workshop.exposed.domain.TestDB
+import io.bluetape4k.workshop.exposed.domain.withDb
+import org.amshove.kluent.shouldBeEqualTo
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.intLiteral
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+class DualTableTest: AbstractExposedTest() {
+
+    /**
+     * 더미 테이블인 "DUAL" 테이블을 이용하는 예제입니다.
+     * 실제 테이블들을 몰라도 쿼리 구문을 실행 할 수 있습니다.
+     *
+     * ```sql
+     * SELECT 1
+     * ```
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun testDualTable(testDb: TestDB) {
+        withDb(testDb) {
+            val resultColumn = intLiteral(1)
+            val result = Table.Dual.select(resultColumn).single()
+            result[resultColumn] shouldBeEqualTo 1
+        }
+    }
+}
