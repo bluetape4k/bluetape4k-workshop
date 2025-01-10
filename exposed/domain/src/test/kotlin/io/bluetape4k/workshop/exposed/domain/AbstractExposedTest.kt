8e99caf42f577232122ce1d8b6fd6c3c9067a887
Debop <sunghyouk.bae@gmail.com>
2025-01-10 17:21:12 +0900

    feature: Entity Cache 예제

--- exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/AbstractExposedTest.kt
+++ exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/AbstractExposedTest.kt
@@ -1,19 +1,13 @@
 package io.bluetape4k.workshop.exposed.domain
 
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
-import io.bluetape4k.jdbc.JdbcDrivers
 import io.bluetape4k.junit5.faker.Fakers
 import io.bluetape4k.logging.KLogging
 import io.bluetape4k.logging.info
 import org.jetbrains.exposed.dao.EntityHook
-import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.Key
 import org.jetbrains.exposed.sql.Schema
 import org.jetbrains.exposed.sql.statements.StatementInterceptor
-import org.junit.jupiter.api.BeforeAll
 import java.util.*
-import javax.sql.DataSource
 
 abstract class AbstractExposedTest {
 
@@ -47,29 +41,29 @@ abstract class AbstractExposedTest {
         }
     }
 
-    protected lateinit var database: Database
-
-    @BeforeAll
-    fun setup() {
-        database = connectDatabase(hikariDataSource())
-    }
-
-    private fun hikariConfigH2(): HikariConfig {
-        return HikariConfig().also {
-            it.driverClassName = JdbcDrivers.DRIVER_CLASS_H2
-            it.jdbcUrl = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=PostgreSQL;"
-            it.username = "sa"
-            it.password = ""
-        }
-    }
-
-    private fun hikariDataSource(): HikariDataSource {
-        return HikariDataSource(hikariConfigH2())
-    }
-
-    private fun connectDatabase(dataSource: DataSource): Database {
-        return Database.connect(dataSource)
-    }
+//    protected lateinit var database: Database
+//
+//    @BeforeAll
+//    fun setup() {
+//        database = connectDatabase(hikariDataSource())
+//    }
+//
+//    private fun hikariConfigH2(): HikariConfig {
+//        return HikariConfig().also {
+//            it.driverClassName = JdbcDrivers.DRIVER_CLASS_H2
+//            it.jdbcUrl = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=PostgreSQL;"
+//            it.username = "sa"
+//            it.password = ""
+//        }
+//    }
+//
+//    private fun hikariDataSource(): HikariDataSource {
+//        return HikariDataSource(hikariConfigH2())
+//    }
+//
+//    private fun connectDatabase(dataSource: DataSource): Database {
+//        return Database.connect(dataSource)
+//    }
 
     fun addIfNotExistsIfSupported() = if (currentDialectTest.supportsIfNotExists) {
         "IF NOT EXISTS "
