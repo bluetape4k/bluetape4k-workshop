8f357c06f6866a1a4d1891d81c1fffa827c6aa02
Debop <sunghyouk.bae@gmail.com>
2025-01-11 15:25:59 +0900

    chore: Add SQL

--- exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/shared/entities/ForeignIdEntityTest.kt
+++ exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/shared/entities/ForeignIdEntityTest.kt
@@ -17,6 +17,7 @@ import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IdTable
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.dao.id.LongIdTable
+import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
@@ -28,22 +29,22 @@ class ForeignIdEntityTest: AbstractExposedTest() {
     object Schema {
 
         object Projects: LongIdTable() {
-            val name = varchar("name", 50)
+            val name: Column<String> = varchar("name", 50)
         }
 
         // one-to-one relationship
         object ProjectConfigs: IdTable<Long>() {
-            override val id = reference("id", Projects)
-            val setting = bool("setting")
+            override val id: Column<EntityID<Long>> = reference("id", Projects)     // one-to-one relationship
+            val setting: Column<Boolean> = bool("setting")
         }
 
         object Actors: IdTable<String>("actors") {
-            override val id = varchar("guild_id", 13).entityId()
+            override val id: Column<EntityID<String>> = varchar("guild_id", 13).entityId()
             override val primaryKey = PrimaryKey(id)
         }
 
         object Roles: IntIdTable("roles") {
-            val actor = reference("guild_id", Actors)
+            val actor: Column<EntityID<String>> = reference("guild_id", Actors)
         }
     }
 
@@ -62,26 +63,42 @@ class ForeignIdEntityTest: AbstractExposedTest() {
     class Actor(id: EntityID<String>): Entity<String>(id) {
         companion object: EntityClass<String, Actor>(Schema.Actors)
 
-        val roles by Role referrersOn Schema.Roles.actor
+        val roles by Role referrersOn Schema.Roles.actor  // one-to-many relationship
     }
 
     class Role(id: EntityID<Int>): IntEntity(id) {
         companion object: IntEntityClass<Role>(Schema.Roles)
 
-        var actor by Actor referencedOn Schema.Roles.actor
+        var actor by Actor referencedOn Schema.Roles.actor  // many-to-one relationship
     }
 
     @ParameterizedTest
     @MethodSource(ENABLE_DIALECTS_METHOD)
-    fun `foreign id entity update`(testDb: TestDB) {
+    fun `foreign id entity update`(testDB: TestDB) {
         withTables(
-            testDb,
+            testDB,
             Schema.Projects, Schema.ProjectConfigs,
             configure = { useNestedTransactions = true }
         ) {
             transaction {
+                /**
+                 * ```sql
+                 * INSERT INTO "SCHEMA$PROJECTS" ("name") VALUES ('Space')
+                 * INSERT INTO "SCHEMA$PROJECTCONFIGS" (ID, SETTING) VALUES (1, TRUE)
+                 * ```
+                 */
                 val projectId = Project.new { name = "Space" }.id.value
+                // ProjectConfig is a one-to-one relationship with Project
                 ProjectConfig.new(projectId) { setting = true }
+
+                /**
+                 * ```sql
+                 * INSERT INTO "SCHEMA$PROJECTS" ("name") VALUES ('Earth')
+                 * INSERT INTO "SCHEMA$PROJECTCONFIGS" (ID, SETTING) VALUES (2, TRUE)
+                 * ```
+                 */
+                val project2 = Project.new { name = "Earth" }
+                ProjectConfig.new(project2.id.value) { setting = true }
             }
 
             transaction {
@@ -96,14 +113,19 @@ class ForeignIdEntityTest: AbstractExposedTest() {
 
     @ParameterizedTest
     @MethodSource(ENABLE_DIALECTS_METHOD)
-    fun `referenced entities with identitical column names`(testDb: TestDB) {
-        withTables(testDb, Schema.Actors, Schema.Roles) {
+    fun `referenced entities with identitical column names`(testDB: TestDB) {
+        withTables(testDB, Schema.Actors, Schema.Roles) {
             val actorA = Actor.new("3746529") { }
             val roleA = Role.new { actor = actorA }
             val roleB = Role.new { actor = actorA }
 
             flushCache()
 
+            /**
+             * ```sql
+             * SELECT ROLES.ID, ROLES.GUILD_ID FROM ROLES WHERE ROLES.GUILD_ID = '3746529'
+             * ```
+             */
             actorA.roles.toList() shouldBeEqualTo listOf(roleA, roleB)
         }
     }
