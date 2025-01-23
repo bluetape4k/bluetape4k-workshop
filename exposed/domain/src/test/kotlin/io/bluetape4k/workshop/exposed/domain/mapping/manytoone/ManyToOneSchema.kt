8e83fa87f737f802d5ad09b2ece10523ce8cbf1a
Debop <sunghyouk.bae@gmail.com>
2025-01-23 22:12:34 +0900

    feat: Many-To-One 예제 추가

---
+++ exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/mapping/manytoone/ManyToOneSchema.kt
@@ -0,0 +1,169 @@
+package io.bluetape4k.workshop.exposed.domain.mapping.manytoone
+
+import io.bluetape4k.workshop.exposed.TestDB
+import io.bluetape4k.workshop.exposed.withTables
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.entityCache
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ReferenceOption.CASCADE
+import org.jetbrains.exposed.sql.SizedIterable
+import org.jetbrains.exposed.sql.Transaction
+
+object ManyToOneSchema {
+
+    object BreweryTable: IntIdTable("brewery") {
+        val name = varchar("name", 255)
+    }
+
+    object BeerTable: IntIdTable("beer") {
+        val name = varchar("name", 255)
+        val brewery = reference("brewery_id", BreweryTable, onDelete = CASCADE, onUpdate = CASCADE)  // many-to-one
+    }
+
+    class Brewery(id: EntityID<Int>): IntEntity(id) {
+        companion object: IntEntityClass<Brewery>(BreweryTable)
+
+        var name by BreweryTable.name
+        val beers: SizedIterable<Beer> by Beer referrersOn BeerTable.brewery   // one-to-many
+
+        override fun equals(other: Any?): Boolean = other is Brewery && id._value == other.id._value
+
+        override fun hashCode(): Int = id._value?.hashCode() ?: System.identityHashCode(this)
+        override fun toString(): String {
+            return "Brewery(id=$id, name=$name)"
+        }
+    }
+
+    class Beer(id: EntityID<Int>): IntEntity(id) {
+        companion object: IntEntityClass<Beer>(BeerTable)
+
+        var name by BeerTable.name
+        var brewery: Brewery by Brewery referencedOn BeerTable.brewery       // many-to-one
+
+        override fun equals(other: Any?): Boolean = other is Beer && id._value == other.id._value
+        override fun hashCode(): Int = id._value?.hashCode() ?: System.identityHashCode(this)
+        override fun toString(): String {
+            return "Beer(id=$id, name=$name)"
+        }
+    }
+
+    fun withBeerTables(
+        testDB: TestDB,
+        statement: Transaction.(brewerys: BreweryTable, beers: BeerTable) -> Unit,
+    ) {
+        withTables(testDB, BreweryTable, BeerTable) {
+            val brewery1 = Brewery.new { name = "Berlin" }
+            val brewery2 = Brewery.new { name = "Munich" }
+
+            val beer1 = Beer.new { name = "Normal"; this.brewery = brewery1 }
+            val beer2 = Beer.new { name = "Special"; this.brewery = brewery1 }
+            val beer3 = Beer.new { name = "Extra"; this.brewery = brewery1 }
+
+            val beer4 = Beer.new { name = "Black"; this.brewery = brewery2 }
+            val beer5 = Beer.new { name = "White"; this.brewery = brewery2 }
+
+            commit()
+            entityCache.clear()
+
+            statement(BreweryTable, BeerTable)
+        }
+    }
+
+    object JugTable: IntIdTable("jugs") {
+        val name = varchar("name", 255)
+    }
+
+    object JugMeterTable: IntIdTable("jug_meters") {
+        val name = varchar("name", 255)
+        val jug = reference("jug_id", JugTable)
+    }
+
+    class Jug(id: EntityID<Int>): IntEntity(id) {
+        companion object: IntEntityClass<Jug>(JugTable)
+
+        var name by JugTable.name
+
+        override fun equals(other: Any?): Boolean = other is Jug && id._value == other.id._value
+        override fun hashCode(): Int = id._value?.hashCode() ?: System.identityHashCode(this)
+        override fun toString(): String {
+            return "Jug(id=$id, name=$name)"
+        }
+    }
+
+    class JugMeter(id: EntityID<Int>): IntEntity(id) {
+        companion object: IntEntityClass<JugMeter>(JugMeterTable)
+
+        var name by JugMeterTable.name
+        var memberOf: Jug by Jug referencedOn JugMeterTable.jug     // many-to-one
+
+        override fun equals(other: Any?): Boolean = other is JugMeter && id._value == other.id._value
+        override fun hashCode(): Int = id._value?.hashCode() ?: System.identityHashCode(this)
+        override fun toString(): String {
+            return "JugMeter(id=$id, name=$name)"
+        }
+    }
+
+    fun withJugTables(
+        testDB: TestDB,
+        statement: Transaction.(jugs: JugTable, jugMeters: JugMeterTable) -> Unit,
+    ) {
+        withTables(testDB, JugTable, JugMeterTable) {
+            val jug = Jug.new { name = "Jug Summer camp" }
+            val emmanuel = JugMeter.new { name = "Emmanuel Bernard"; this.memberOf = jug }
+            val jerome = JugMeter.new { name = "Jerome"; this.memberOf = jug }
+
+            commit()
+            entityCache.clear()
+
+            statement(JugTable, JugMeterTable)
+        }
+    }
+
+
+    object SalesForceTable: IntIdTable("sales_forces") {
+        val name = varchar("name", 255)
+    }
+
+    object SalesGuyTable: IntIdTable("sales_guys") {
+        val name = varchar("name", 255)
+        val salesForce = reference("sales_force_id", SalesForceTable, onDelete = CASCADE, onUpdate = CASCADE)
+    }
+
+    class SalesForce(id: EntityID<Int>): IntEntity(id) {
+        companion object: IntEntityClass<SalesForce>(SalesForceTable)
+
+        var name by SalesForceTable.name
+        val salesGuys: SizedIterable<SalesGuy> by SalesGuy referrersOn SalesGuyTable.salesForce // one-to-many
+
+        override fun equals(other: Any?): Boolean = other is SalesForce && id._value == other.id._value
+        override fun hashCode(): Int = id._value?.hashCode() ?: System.identityHashCode(this)
+        override fun toString(): String {
+            return "SalesForce(id=$id, name=$name)"
+        }
+    }
+
+    class SalesGuy(id: EntityID<Int>): IntEntity(id) {
+        companion object: IntEntityClass<SalesGuy>(SalesGuyTable)
+
+        var name by SalesGuyTable.name
+        var salesForce: SalesForce by SalesForce referencedOn SalesGuyTable.salesForce    // many-to-one
+
+        override fun equals(other: Any?): Boolean = other is SalesGuy && id._value == other.id._value
+        override fun hashCode(): Int = id._value?.hashCode() ?: System.identityHashCode(this)
+        override fun toString(): String {
+            return "SalesGuy(id=$id, name=$name)"
+        }
+    }
+
+    fun withSalesTables(
+        testDB: TestDB,
+        statement: Transaction.(salesForces: SalesForceTable, salesGuys: SalesGuyTable) -> Unit,
+    ) {
+        withTables(testDB, SalesForceTable, SalesGuyTable) {
+            statement(SalesForceTable, SalesGuyTable)
+        }
+    }
+
+}
---
+++ exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/mapping/manytoone/ManyToOneTest.kt
@@ -0,0 +1,153 @@
+package io.bluetape4k.workshop.exposed.domain.mapping.manytoone
+
+import io.bluetape4k.collections.size
+import io.bluetape4k.workshop.exposed.AbstractExposedTest
+import io.bluetape4k.workshop.exposed.TestDB
+import io.bluetape4k.workshop.exposed.domain.mapping.manytoone.ManyToOneSchema.Brewery
+import io.bluetape4k.workshop.exposed.domain.mapping.manytoone.ManyToOneSchema.Jug
+import io.bluetape4k.workshop.exposed.domain.mapping.manytoone.ManyToOneSchema.JugMeter
+import io.bluetape4k.workshop.exposed.domain.mapping.manytoone.ManyToOneSchema.SalesForce
+import io.bluetape4k.workshop.exposed.domain.mapping.manytoone.ManyToOneSchema.SalesGuy
+import io.bluetape4k.workshop.exposed.domain.mapping.manytoone.ManyToOneSchema.withBeerTables
+import io.bluetape4k.workshop.exposed.domain.mapping.manytoone.ManyToOneSchema.withJugTables
+import io.bluetape4k.workshop.exposed.domain.mapping.manytoone.ManyToOneSchema.withSalesTables
+import org.amshove.kluent.shouldBeEqualTo
+import org.amshove.kluent.shouldBeGreaterThan
+import org.amshove.kluent.shouldHaveSize
+import org.jetbrains.exposed.dao.entityCache
+import org.jetbrains.exposed.dao.flushCache
+import org.jetbrains.exposed.dao.load
+import org.jetbrains.exposed.dao.with
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.deleteAll
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.selectAll
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+class ManyToOneTest: AbstractExposedTest() {
+
+    /**
+     *
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `many-to-one unidirectional`(testDB: TestDB) {
+        withJugTables(testDB) { jugs, jugMeters ->
+            val jug = Jug.findById(1)!!
+
+            val emmanuel = JugMeter.findById(1)!!
+            emmanuel.memberOf shouldBeEqualTo jug
+
+            val jerome = JugMeter.findById(2)!!
+            jerome.memberOf shouldBeEqualTo jug
+
+            jugMeters.deleteAll()
+            entityCache.clear()
+
+            jugs.selectAll().count() shouldBeEqualTo 1
+            JugMeter.all().count() shouldBeEqualTo 0
+
+            jugs.deleteWhere { jugs.id eq 1 }
+            Jug.all().count() shouldBeEqualTo 0
+        }
+    }
+
+    /**
+     * ```sql
+     * SELECT BREWERY.ID, BREWERY."name" FROM BREWERY
+     * SELECT BEER.ID, BEER."name", BEER.BREWERY_ID FROM BEER WHERE BEER.BREWERY_ID = 1
+     * ```
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `many-to-one bidirectional lazy loading`(testDB: TestDB) {
+        withBeerTables(testDB) { brewerys, beers ->
+            val brewery1 = Brewery.findById(1)!!
+
+            // Fetch lazy loading
+            val loaded = Brewery.all().first()
+            loaded shouldBeEqualTo brewery1
+            loaded.beers shouldHaveSize 3
+        }
+    }
+
+    /**
+     * ```sql
+     * SELECT BREWERY.ID, BREWERY."name" FROM BREWERY
+     * SELECT BEER.ID, BEER."name", BEER.BREWERY_ID FROM BEER WHERE BEER.BREWERY_ID IN (1, 2)
+     * ```
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `many-to-one bidirectional eager loading`(testDB: TestDB) {
+        withBeerTables(testDB) { brewerys, beers ->
+            val berlin = Brewery.findById(1)!!
+
+            // fetch eager loading
+            val loadedBrewerys = Brewery.all().with(Brewery::beers)
+            val loaded = loadedBrewerys.first()
+            loaded shouldBeEqualTo berlin
+            loaded.beers shouldHaveSize 3
+
+            loadedBrewerys.forEach {
+                it.beers.size() shouldBeGreaterThan 0
+            }
+        }
+    }
+
+    /**
+     * ```sql
+     * DELETE FROM BEER WHERE BEER.ID = 1
+     * SELECT BREWERY.ID, BREWERY."name" FROM BREWERY WHERE BREWERY.ID = 1
+     * SELECT BEER.ID, BEER."name", BEER.BREWERY_ID FROM BEER WHERE BEER.BREWERY_ID = 1
+     * ```
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `many-to-one bidirectional - remove details`(testDB: TestDB) {
+        withBeerTables(testDB) { brewerys, beers ->
+            val brewery = Brewery.findById(1)!!
+            brewery.beers shouldHaveSize 3
+
+            val beerToRemove = brewery.beers.first()
+            beerToRemove.delete()
+
+            entityCache.clear()
+            Brewery.findById(1)!!.beers shouldHaveSize 2
+        }
+    }
+
+    /**
+     * ```sql
+     * SELECT SALES_FORCES.ID, SALES_FORCES."name" FROM SALES_FORCES WHERE SALES_FORCES.ID = 1
+     * SELECT SALES_GUYS.ID, SALES_GUYS."name", SALES_GUYS.SALES_FORCE_ID FROM SALES_GUYS WHERE SALES_GUYS.SALES_FORCE_ID = 1
+     * ```
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `many-to-one bidirectional with cascade all`(testDB: TestDB) {
+        withSalesTables(testDB) { salesForces, salesGuys ->
+            val salesForce = SalesForce.new { name = "BMW Korea" }
+            val salesGuy1 = SalesGuy.new { name = "debop"; this.salesForce = salesForce }
+            val salesGuy2 = SalesGuy.new { name = "smith"; this.salesForce = salesForce }
+            val salesGuy3 = SalesGuy.new { name = "james"; this.salesForce = salesForce }
+
+            flushCache()
+            entityCache.clear()
+
+            val loaded = SalesForce.findById(salesForce.id)!!.load(SalesForce::salesGuys)
+            loaded shouldBeEqualTo salesForce
+            loaded.salesGuys.toList() shouldBeEqualTo listOf(salesGuy1, salesGuy2, salesGuy3)
+
+            val guyToRemove = loaded.salesGuys.last()
+            guyToRemove.delete()
+
+            entityCache.clear()
+
+            val loaded2 = SalesForce.findById(salesForce.id)!!.load(SalesForce::salesGuys)
+            loaded2 shouldBeEqualTo salesForce
+            loaded2.salesGuys.toList() shouldBeEqualTo listOf(salesGuy1, salesGuy2)
+        }
+    }
+}
