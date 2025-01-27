7f102be6a5838b8957d9ee5a4c541960aea34f50
Debop <sunghyouk.bae@gmail.com>
2025-01-27 14:58:58 +0900

    feat: CustomId 예제

---
+++ exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/mapping/customId/CustomIdTest.kt
@@ -0,0 +1,237 @@
+package io.bluetape4k.workshop.exposed.domain.mapping.customId
+
+import io.bluetape4k.logging.KLogging
+import io.bluetape4k.logging.debug
+import io.bluetape4k.workshop.exposed.AbstractExposedTest
+import io.bluetape4k.workshop.exposed.TestDB
+import io.bluetape4k.workshop.exposed.withDb
+import io.bluetape4k.workshop.exposed.withTables
+import org.amshove.kluent.shouldBeEqualTo
+import org.amshove.kluent.shouldBeTrue
+import org.amshove.kluent.shouldNotBeNull
+import org.jetbrains.exposed.dao.Entity
+import org.jetbrains.exposed.dao.EntityClass
+import org.jetbrains.exposed.dao.entityCache
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IdTable
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.exists
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.selectAll
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+
+class CustomIdTest: AbstractExposedTest() {
+
+    companion object: KLogging()
+
+    /**
+     * Custom Type 의 Id 를 가지는 Table
+     *
+     * ```sql
+     * CREATE TABLE IF NOT EXISTS EMAILS (
+     *      EMAIL_ID VARCHAR(64) NOT NULL,
+     *      "name" VARCHAR(255) NOT NULL,
+     *      SSN CHAR(14) NOT NULL
+     * )
+     * ```
+     */
+    object CustomIdTable: IdTable<Email>("emails") {
+        override val id: Column<EntityID<Email>> = email("email_id").entityId()
+        val name: Column<String> = varchar("name", 255)
+        val ssn: Column<Ssn> = ssn("ssn").uniqueIndex()
+    }
+
+
+    class CustomIdEntity(id: EntityID<Email>): Entity<Email>(id) {
+        companion object: EntityClass<Email, CustomIdEntity>(CustomIdTable)
+
+        var name by CustomIdTable.name
+        var ssn by CustomIdTable.ssn
+
+        override fun equals(other: Any?): Boolean = other is CustomIdEntity && id._value == other.id._value
+        override fun hashCode(): Int = id._value?.hashCode() ?: System.identityHashCode(this)
+        override fun toString(): String = "CustomIdEntity(id=${id._value}, name=$name, ssn=$ssn)"
+    }
+
+    private fun newEntity(): CustomIdEntity {
+        val email = Email(faker.internet().emailAddress())
+        val name = faker.name().name()
+        val ssn = faker.idNumber().ssnValid()
+
+        return CustomIdEntity.new(email) {
+            this.name = name
+            this.ssn = Ssn(ssn)
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `create schema`(testDB: TestDB) {
+        withDb(testDB) {
+            SchemaUtils.create(CustomIdTable)
+            CustomIdTable.exists().shouldBeTrue()
+            SchemaUtils.drop(CustomIdTable)
+        }
+    }
+
+    /**
+     * Custom Type 의 Id 를 가지는 Table 에 레코드를 저장합니다.
+     *
+     * ```sql
+     * INSERT INTO EMAILS (EMAIL_ID, "name", SSN)
+     * VALUES (gwyn.jerde@gmail.com, 'Wes Toy', 211-99-4075)
+     * ```
+     * ```sql
+     * SELECT EMAILS.EMAIL_ID,
+     *        EMAILS."name",
+     *        EMAILS.SSN
+     *   FROM EMAILS
+     *  WHERE EMAILS.EMAIL_ID = gwyn.jerde@gmail.com
+     * ```
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `save custom id record`(testDB: TestDB) {
+        withTables(testDB, CustomIdTable) {
+            val entityId = CustomIdTable.insertAndGetId { row ->
+                row[CustomIdTable.id] = Email(faker.internet().emailAddress())
+                row[CustomIdTable.name] = faker.name().name()
+                row[CustomIdTable.ssn] = Ssn(faker.idNumber().ssnValid())
+            }
+            entityId.value.value.shouldNotBeNull()
+
+            val row = CustomIdTable.selectAll()
+                .where { CustomIdTable.id eq entityId }
+                .single()
+
+            row[CustomIdTable.id].value shouldBeEqualTo entityId.value
+        }
+    }
+
+    /**
+     * Custom Type 의 Id 를 가지는 Entity 를 저장합니다.
+     *
+     * ```sql
+     * INSERT INTO EMAILS (EMAIL_ID, "name", SSN)
+     * VALUES (clarice.bednar@gmail.com, 'Mr. Ludie Mosciski', 639-15-7761)
+     * ```
+     *
+     * ```sql
+     * SELECT EMAILS.EMAIL_ID,
+     *        EMAILS."name",
+     *        EMAILS.SSN
+     *   FROM EMAILS
+     *  WHERE EMAILS.EMAIL_ID = clarice.bednar@gmail.com
+     * ```
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `save custom id entity`(testDB: TestDB) {
+        withTables(testDB, CustomIdTable) {
+            val entity = newEntity()
+            entity.id.shouldNotBeNull()
+
+            entityCache.clear()
+
+            val loaded = CustomIdEntity.findById(entity.id)
+
+            loaded shouldBeEqualTo entity
+        }
+    }
+
+    /**
+     * Custom Type 의 Id 를 가지는 Entity 를 저장하고, Ssn 속성으로 조회합니다.
+     *
+     * ```sql
+     * SELECT EMAILS.EMAIL_ID, EMAILS."name", EMAILS.SSN
+     *   FROM EMAILS
+     *  WHERE EMAILS.SSN = 687-96-8924
+     * ```
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `find by custom column type`(testDB: TestDB) {
+        withTables(testDB, CustomIdTable) {
+            val entity = newEntity()
+            entity.id.shouldNotBeNull()
+
+            entityCache.clear()
+
+            val loaded = CustomIdEntity.find { CustomIdTable.ssn eq entity.ssn }.singleOrNull()
+            log.debug { "loaded=$loaded" }
+            loaded shouldBeEqualTo entity
+        }
+    }
+
+    /**
+     * Custom Type 의 Id 를 가지는 Entity 를 저장하고, Id 로 조회합니다.
+     *
+     * ```sql
+     * SELECT EMAILS.EMAIL_ID,
+     *        EMAILS."name",
+     *        EMAILS.SSN
+     *   FROM EMAILS
+     *  WHERE EMAILS.EMAIL_ID IN (clyde.gutkowski@hotmail.com,
+     *                            amos.bosco@yahoo.com,
+     *                            charlena.gulgowski@gmail.com,
+     *                            sherika.considine@gmail.com,
+     *                            elenora.aufderhar@hotmail.com)
+     *  ORDER BY EMAILS."name" ASC
+     * ```
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `find records by custom ids`(testDB: TestDB) {
+        withTables(testDB, CustomIdTable) {
+            val entities = List(5) { newEntity() }
+
+            entityCache.clear()
+
+            val rows = CustomIdTable.selectAll()
+                .where { CustomIdTable.id inList entities.map { it.id } }
+                .orderBy(CustomIdTable.name to SortOrder.ASC)
+
+            val loaded = CustomIdEntity.wrapRows(rows).toList()
+
+            loaded shouldBeEqualTo entities.sortedBy { it.name }
+        }
+    }
+
+    /**
+     * Custom Type 의 Id 를 가지는 Entity 를 저장하고, Id 로 조회합니다.
+     *
+     * ```sql
+     * SELECT emails.email_id,
+     *        emails."name",
+     *        emails.ssn
+     *   FROM emails
+     *  WHERE emails.email_id IN (fiona.thompson@yahoo.com,
+     *                            michael.cruickshank@gmail.com,
+     *                            enrique.kertzmann@yahoo.com,
+     *                            garland.rippin@gmail.com,
+     *                            magdalena.gleason@gmail.com)
+     *  ORDER BY emails."name" ASC
+     * ```
+     *
+     */
+    @ParameterizedTest
+    @MethodSource(ENABLE_DIALECTS_METHOD)
+    fun `find entities by custom ids`(testDB: TestDB) {
+        withTables(testDB, CustomIdTable) {
+            val entities = List(5) { newEntity() }
+
+            entityCache.clear()
+
+            val loaded = CustomIdEntity
+                .find { CustomIdTable.id inList entities.map { it.id } }
+                .orderBy(CustomIdTable.name to SortOrder.ASC)
+                .toList()
+
+            loaded shouldBeEqualTo entities.sortedBy { it.name }
+        }
+    }
+}
---
+++ exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/mapping/customId/SsnColumn.kt
@@ -0,0 +1,29 @@
+package io.bluetape4k.workshop.exposed.domain.mapping.customId
+
+import org.jetbrains.exposed.sql.CharColumnType
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.ColumnTransformer
+import org.jetbrains.exposed.sql.ColumnWithTransform
+import org.jetbrains.exposed.sql.Table
+import java.io.Serializable
+
+
+@JvmInline
+value class Ssn(val value: String): Serializable {
+    companion object {
+        val EMPTY = Ssn("")
+        const val SSN_LENGTH = 14
+    }
+}
+
+fun Table.ssn(name: String, length: Int = 14): Column<Ssn> =
+    registerColumn(name, SsnColumnType(length))
+
+open class SsnColumnType(
+    val length: Int = Ssn.SSN_LENGTH,
+): ColumnWithTransform<String, Ssn>(CharColumnType(length), StringToSsnTransformer())
+
+class StringToSsnTransformer: ColumnTransformer<String, Ssn> {
+    override fun unwrap(ssn: Ssn): String = ssn.value
+    override fun wrap(value: String): Ssn = Ssn(value)
+}
