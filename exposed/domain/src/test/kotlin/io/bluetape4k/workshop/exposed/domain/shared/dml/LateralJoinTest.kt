fe7eeb77f4d9f5c32dc5bfaf23ed5d8d0a70069b
Debop <sunghyouk.bae@gmail.com>
2025-01-29 06:55:31 +0900

    chore: Add SQL comments

--- exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/shared/dml/LateralJoinTest.kt
+++ exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/shared/dml/LateralJoinTest.kt
@@ -6,11 +6,12 @@ import io.bluetape4k.workshop.exposed.expectException
 import io.bluetape4k.workshop.exposed.withTables
 import org.amshove.kluent.shouldBeEqualTo
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.Join
 import org.jetbrains.exposed.sql.JoinType
 import org.jetbrains.exposed.sql.JoinType.LEFT
 import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.alias
-import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.batchInsert
 import org.jetbrains.exposed.sql.insertAndGetId
 import org.jetbrains.exposed.sql.joinQuery
 import org.jetbrains.exposed.sql.lastQueryAlias
@@ -26,22 +27,27 @@ class LateralJoinTest: AbstractExposedTest() {
      * ### Lateral Join Query
      *
      * ```sql
-     * SELECT literal_join_parent.id, literal_join_parent."value", q0.id, q0.tester1, q0."value"
-     *   FROM literal_join_parent
-     *   CROSS JOIN LATERAL (SELECT literal_join_child.id, literal_join_child.tester1, literal_join_child."value"
-     *                         FROM literal_join_child
-     *                        WHERE literal_join_child."value" > literal_join_parent."value" LIMIT 1
-     *                      ) q0
+     * SELECT parent.id,
+     *        parent."value",
+     *        q0.id,
+     *        q0.parent_id,
+     *        q0."value"
+     *   FROM parent CROSS JOIN LATERAL
+     *          (SELECT child.id,
+     *                  child.parent_id,
+     *                  child."value"
+     *             FROM child
+     *            WHERE child."value" > parent."value"
+     *            LIMIT 1
+     *          ) q0
      * ```
      */
     @ParameterizedTest
     @FieldSource("lateralJoinSupportedDb")
-    fun `lateral join query`(dialect: TestDB) {
-        withTestTablesAndDefaultData(dialect) { parent, child, _ ->
-            val query = parent.joinQuery(
-                joinType = JoinType.CROSS,
-                lateral = true
-            ) {
+    fun `lateral join query`(testDB: TestDB) {
+        withTestTablesAndDefaultData(testDB) { parent, child, _ ->
+
+            val query = parent.joinQuery(joinType = JoinType.CROSS, lateral = true) {
                 child.selectAll().where { child.value greater parent.value }.limit(1)
             }
 
@@ -55,10 +61,32 @@ class LateralJoinTest: AbstractExposedTest() {
     @FieldSource("lateralJoinSupportedDb")
     fun `lateral join query alias`(dialect: TestDB) {
         withTestTablesAndDefaultData(dialect) { parent, child, _ ->
-            // Cross join
-            child.selectAll().where { child.value greater parent.value }.limit(1).alias("subquery")
+
+            /**
+             * Cross Join Lateral Query
+             *
+             * ```sql
+             * SELECT parent.id,
+             *        parent."value",
+             *        child1.id,
+             *        child1.parent_id,
+             *        child1."value"
+             *   FROM parent CROSS JOIN LATERAL
+             *          (SELECT child.id,
+             *                  child.parent_id,
+             *                  child."value"
+             *             FROM child
+             *            WHERE child."value" > parent."value"
+             *            LIMIT 1
+             *          ) child1
+             * ```
+             */
+            child.selectAll()
+                .where { child.value greater parent.value }
+                .limit(1)
+                .alias("child1")
                 .let { subqueryAlias ->
-                    val query = parent.join(
+                    val join = parent.join(
                         subqueryAlias,
                         JoinType.CROSS,
                         onColumn = parent.id,
@@ -66,13 +94,32 @@ class LateralJoinTest: AbstractExposedTest() {
                         lateral = true
                     )
 
-                    query.selectAll().map { it[subqueryAlias[child.value]] } shouldBeEqualTo listOf(30)
+                    join.selectAll().map { it[subqueryAlias[child.value]] } shouldBeEqualTo listOf(30)
                 }
 
-            // Left join
-            child.selectAll().where { child.value greater parent.value }.alias("subquery")
+            /**
+             * Left Join Lateral Query
+             *
+             * ```sql
+             * SELECT parent.id,
+             *        parent."value",
+             *        child1.id,
+             *        child1.parent_id,
+             *        child1."value"
+             *   FROM parent LEFT JOIN LATERAL
+             *          (SELECT child.id,
+             *                  child.parent_id,
+             *                  child."value"
+             *             FROM child
+             *            WHERE child."value" > parent."value"
+             *          ) child1 ON parent.id = child1.parent_id
+             * ```
+             */
+            child.selectAll()
+                .where { child.value greater parent.value }
+                .alias("child1")
                 .let { subqueryAlias ->
-                    val query = parent.join(
+                    val join = parent.join(
                         subqueryAlias,
                         JoinType.LEFT,
                         onColumn = parent.id,
@@ -80,14 +127,36 @@ class LateralJoinTest: AbstractExposedTest() {
                         lateral = true
                     )
 
-                    query.selectAll().map { it[subqueryAlias[child.value]] } shouldBeEqualTo listOf(30)
+                    join.selectAll().map { it[subqueryAlias[child.value]] } shouldBeEqualTo listOf(30)
                 }
 
-            // Left join to Alias
-            val parentQuery = parent.selectAll().alias("parent_query")
-            child.selectAll().where { child.value greater parentQuery[parent.value] }.alias("subquery")
+            /**
+             * Left join to Alias
+             *
+             * ```sql
+             * SELECT parent1.id,
+             *        parent1."value",
+             *        child1.id,
+             *        child1.parent_id,
+             *        child1."value"
+             *   FROM (SELECT parent.id,
+             *                parent."value"
+             *           FROM parent) parent1
+             *         LEFT JOIN LATERAL
+             *         (SELECT child.id,
+             *                 child.parent_id,
+             *                 child."value"
+             *            FROM child
+             *           WHERE child."value" > parent1."value") child1
+             *         ON parent1.id = child1.parent_id
+             * ```
+             */
+            val parentQuery = parent.selectAll().alias("parent1")
+            child.selectAll()
+                .where { child.value greater parentQuery[parent.value] }
+                .alias("child1")
                 .let { subqueryAlias ->
-                    val query = parentQuery.join(
+                    val join: Join = parentQuery.join(
                         subqueryAlias,
                         JoinType.LEFT,
                         onColumn = parentQuery[parent.id],
@@ -95,7 +164,7 @@ class LateralJoinTest: AbstractExposedTest() {
                         lateral = true
                     )
 
-                    query.selectAll().map { it[subqueryAlias[child.value]] } shouldBeEqualTo listOf(30)
+                    join.selectAll().map { it[subqueryAlias[child.value]] } shouldBeEqualTo listOf(30)
                 }
         }
     }
@@ -104,25 +173,45 @@ class LateralJoinTest: AbstractExposedTest() {
     @FieldSource("lateralJoinSupportedDb")
     fun `lateral direct table join`(dialect: TestDB) {
         withTestTables(dialect) { parent, child, _ ->
-            // Explicit notation
+            // Lateral 적용 시, 명시적으로 테이블 간의 JOIN 조건의 컬럼을 지정하면 예외가 발생합니다. (쿼리를 사용해야 합니다)
             expectException<IllegalArgumentException> {
                 parent.join(child, LEFT, onColumn = parent.id, otherColumn = child.parent, lateral = true)
             }
 
-            // Implicit notation
+            // Lateral 적용 시, 암묵적인 테이블 간의 JOIN 조건의 컬럼을 지정하면 예외가 발생합니다. (쿼리를 사용해야 합니다)
             expectException<IllegalArgumentException> {
                 parent.join(child, LEFT, lateral = true).selectAll().toList()
             }
         }
     }
 
-
-    object Parent: IntIdTable("literal_join_parent") {
+    /**
+     * ```sql
+     * CREATE TABLE IF NOT EXISTS parent (
+     *      id SERIAL PRIMARY KEY,
+     *      "value" INT NOT NULL
+     * )
+     * ```
+     */
+    object Parent: IntIdTable("parent") {
         val value = integer("value")
     }
 
-    object Child: IntIdTable("literal_join_child") {
-        val parent = reference("tester1", Parent.id)
+    /**
+     * ```sql
+     * CREATE TABLE IF NOT EXISTS child (
+     *      id SERIAL PRIMARY KEY,
+     *      parent_id INT NOT NULL,
+     *      "value" INT NOT NULL,
+     *
+     *      CONSTRAINT fk_child_parent_id__id
+     *          FOREIGN KEY (parent_id) REFERENCES parent(id)
+     *          ON DELETE RESTRICT ON UPDATE RESTRICT
+     * )
+     * ```
+     */
+    object Child: IntIdTable("child") {
+        val parent = reference("parent_id", Parent.id)
         val value = integer("value")
     }
 
@@ -133,17 +222,15 @@ class LateralJoinTest: AbstractExposedTest() {
     }
 
     private fun withTestTablesAndDefaultData(
-        dialect: TestDB,
+        testDB: TestDB,
         statement: Transaction.(Parent, Child, TestDB) -> Unit,
     ) {
-        withTestTables(dialect) { parent, child, testDb ->
+        withTestTables(testDB) { parent, child, testDb ->
             val id = parent.insertAndGetId { it[value] = 20 }
 
-            listOf(10, 30).forEach { value ->
-                child.insert {
-                    it[child.parent] = id
-                    it[child.value] = value
-                }
+            child.batchInsert(listOf(10, 30)) { value ->
+                this[child.parent] = id
+                this[child.value] = value
             }
 
             statement(parent, child, testDb)
