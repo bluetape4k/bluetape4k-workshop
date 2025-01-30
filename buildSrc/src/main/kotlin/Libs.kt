3a4ab74dd3e59eee8ec43ba3bf6c4893045f113c
Debop <sunghyouk.bae@gmail.com>
2025-01-30 11:20:28 +0900

    build(dep): Bump up kotlin (2.1.0 -> 2.1.10)

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -63,7 +63,7 @@ object Versions {
     // Java 21, Kotlin 2.0 이상에서 사용하세요
     const val bluetape4k = "0.0.3-SNAPSHOT"              // Bluetape4k Github Package 로 배포됩니다.
 
-    const val kotlin = "2.1.0"                 // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
+    const val kotlin = "2.1.10"                 // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
     const val kotlinx_coroutines = "1.10.1"      // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
     const val kotlinx_serialization = "1.7.3"   // https://mvnrepository.com/search?q=kotlinx-serialization
 
