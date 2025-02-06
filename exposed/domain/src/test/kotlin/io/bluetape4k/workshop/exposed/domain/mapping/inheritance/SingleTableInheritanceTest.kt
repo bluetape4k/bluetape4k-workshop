0524920ad0994a720a328638baffbed5df25f99a
Debop <sunghyouk.bae@gmail.com>
2025-02-06 18:48:16 +0900

    chore: Add SQL comments

--- exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/mapping/inheritance/SingleTableInheritanceTest.kt
+++ exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/mapping/inheritance/SingleTableInheritanceTest.kt
@@ -13,6 +13,7 @@ import org.jetbrains.exposed.dao.entityCache
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.andWhere
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.javatime.date
 import org.junit.jupiter.params.ParameterizedTest
@@ -21,26 +22,30 @@ import org.junit.jupiter.params.provider.MethodSource
 class SingleTableInheritanceTest: AbstractExposedTest() {
 
     /**
+     * 한 테이블에 2가지 종류의 컬럼 정보를 저장하는 테이블
+     *
      * ```sql
-     * CREATE TABLE IF NOT EXISTS BILLING (
-     *      ID INT AUTO_INCREMENT PRIMARY KEY,
-     *      OWNER VARCHAR(255) NOT NULL,
-     *      SWIFT VARCHAR(255) NOT NULL,
+     * CREATE TABLE IF NOT EXISTS billing (
+     *      -- 공통 속성
+     *      id SERIAL PRIMARY KEY,
+     *      "owner" VARCHAR(255) NOT NULL,
+     *      swift VARCHAR(255) NOT NULL,
+     *      dtype VARCHAR(31) DEFAULT 'UNKNOWN' NOT NULL,
      *
-     *      DTYPE VARCHAR(31) DEFAULT 'UNKNOWN' NOT NULL,
+     *      -- CreditCard 속성
+     *      card_number VARCHAR(32) NULL,
+     *      company_name VARCHAR(255) NULL,
+     *      exp_month INT NULL,
+     *      exp_year INT NULL,
+     *      start_date DATE NULL,
+     *      end_date DATE NULL,
      *
-     *      CARD_NUMBER VARCHAR(32) NULL,
-     *      COMPANY_NAME VARCHAR(255) NULL,
-     *      EXP_MONTH INT NULL,
-     *      EXP_YEAR INT NULL,
-     *      START_DATE DATE NULL,
-     *      END_DATE DATE NULL,
-     *
-     *      ACCOUNT_NUMBER VARCHAR(255) NULL,
-     *      BANK_NAME VARCHAR(255) NULL
+     *      -- BankAccount 속성
+     *      account_number VARCHAR(255) NULL,
+     *      bank_name VARCHAR(255) NULL
      * );
      *
-     * CREATE INDEX BILLING_OWNER ON BILLING (OWNER);
+     * CREATE INDEX billing_owner ON billing ("owner");
      * ```
      */
     object BillingTable: IntIdTable("billing") {
@@ -72,7 +77,6 @@ class SingleTableInheritanceTest: AbstractExposedTest() {
     abstract class Billing(id: EntityID<Int>): IntEntity(id) {
         var owner by BillingTable.owner
         var swift by BillingTable.swift
-
         var dtype: BillingType by BillingTable.dtype
 
         override fun equals(other: Any?): Boolean = idEquals(other)
@@ -80,6 +84,8 @@ class SingleTableInheritanceTest: AbstractExposedTest() {
         override fun toString(): String {
             return toStringBuilder()
                 .add("owner", owner)
+                .add("swift", swift)
+                .add("dtype", dtype)
                 .toString()
         }
     }
@@ -90,6 +96,29 @@ class SingleTableInheritanceTest: AbstractExposedTest() {
                 return super.new(id, init).apply {
                     dtype = BillingType.CREDIT_CARD
                 }
+            }
+
+            /**
+             * CreditCard 엔티티에 해당하는 정보 중 해당 id 값을 가지는 엔티티를 조회합니다.
+             */
+            override fun findById(id: EntityID<Int>): CreditCard? {
+                val query = BillingTable
+                    .select(
+                        BillingTable.id,
+                        BillingTable.owner,
+                        BillingTable.swift,
+                        BillingTable.dtype,
+                        BillingTable.cardNumber,
+                        BillingTable.companyName,
+                        BillingTable.expMonth,
+                        BillingTable.expYear,
+                        BillingTable.startDate,
+                        BillingTable.endDate,
+                    )
+                    .where { BillingTable.id eq id }
+                    .andWhere { BillingTable.dtype eq BillingType.CREDIT_CARD }
+
+                return CreditCard.wrapRows(query).singleOrNull()
             }
 
             fun countCreditCard(): Long = super.count(BillingTable.dtype eq BillingType.CREDIT_CARD)
@@ -108,6 +137,8 @@ class SingleTableInheritanceTest: AbstractExposedTest() {
         override fun toString(): String =
             toStringBuilder()
                 .add("owner", owner)
+                .add("swift", swift)
+                .add("dtype", dtype)
                 .add("cardNumber", cardNumber)
                 .add("companyName", companyName)
                 .add("expMonth", expMonth)
@@ -125,6 +156,26 @@ class SingleTableInheritanceTest: AbstractExposedTest() {
                 }
             }
 
+            /**
+             * BankAccount 엔티티에 해당하는 정보 중 해당 id 값을 가지는 엔티티를 조회합니다.
+             */
+            override fun findById(id: EntityID<Int>): BankAccount? {
+                val query = BillingTable
+                    .select(
+                        BillingTable.id,
+                        BillingTable.owner,
+                        BillingTable.swift,
+                        BillingTable.dtype,
+                        BillingTable.accountNumber,
+                        BillingTable.bankName
+                    )
+                    .where { BillingTable.id eq id }
+                    .andWhere { BillingTable.dtype eq BillingType.BANK_ACCOUNT }
+
+                return BankAccount.wrapRows(query).singleOrNull()
+            }
+
+
             fun countBankAccount(): Long = super.count(BillingTable.dtype eq BillingType.BANK_ACCOUNT)
         }
 
@@ -135,6 +186,8 @@ class SingleTableInheritanceTest: AbstractExposedTest() {
         override fun hashCode(): Int = idHashCode()
         override fun toString(): String = toStringBuilder()
             .add("owner", owner)
+            .add("swift", swift)
+            .add("dtype", dtype)
             .add("accountNumber", accountNumber)
             .add("bankName", bankName)
             .toString()
@@ -144,6 +197,13 @@ class SingleTableInheritanceTest: AbstractExposedTest() {
     @MethodSource(ENABLE_DIALECTS_METHOD)
     fun `single table with tow type entities`(testDB: TestDB) {
         withTables(testDB, BillingTable) {
+            /**
+             * ```sql
+             * -- Insert BankAccount
+             * INSERT INTO billing ("owner", swift, account_number, bank_name, dtype, card_number, company_name, exp_month, exp_year)
+             * VALUES ('debop', 'NACFKRSE', '123-456-7890', 'Kookmin Bank', 'BANK_ACCOUNT', NULL, NULL, NULL, NULL)
+             * ```
+             */
             val account = BankAccount.new {
                 owner = "debop"
                 swift = "NACFKRSE"
@@ -151,6 +211,13 @@ class SingleTableInheritanceTest: AbstractExposedTest() {
                 bankName = "Kookmin Bank"
             }
 
+            /**
+             * ```sql
+             * -- Insrt CreditCard
+             * INSERT INTO billing ("owner", swift, account_number, bank_name, dtype, card_number, company_name, exp_month, exp_year)
+             * VALUES ('debop', 'NACFKRSE', NULL, NULL, 'CREDIT_CARD', '1234-5678-9012-3456', 'VISA', 12, 2023)
+             * ```
+             */
             val card = CreditCard.new {
                 owner = "debop"
                 swift = "NACFKRSE"
@@ -162,21 +229,85 @@ class SingleTableInheritanceTest: AbstractExposedTest() {
 
             entityCache.clear()
 
+            /**
+             * ```sql
+             * -- Select BankAccount
+             * SELECT billing.id,
+             *        billing."owner",
+             *        billing.swift,
+             *        billing.dtype,
+             *        billing.account_number,
+             *        billing.bank_name
+             *   FROM billing
+             *  WHERE (billing.id = 1)
+             *    AND (billing.dtype = 'BANK_ACCOUNT')
+             * ```
+             */
             val account2 = BankAccount.findById(account.id)!!
             account2 shouldBeEqualTo account
 
+            /**
+             * ```sql
+             * SELECT billing.id,
+             *        billing."owner",
+             *        billing.swift,
+             *        billing.dtype,
+             *        billing.card_number,
+             *        billing.company_name,
+             *        billing.exp_month,
+             *        billing.exp_year,
+             *        billing.start_date,
+             *        billing.end_date,
+             *   FROM billing
+             *  WHERE billing.id = 2
+             *    AND billing.dtype = 'CREDIT_CARD'
+             * ```
+             */
             val card2 = CreditCard.findById(card.id)!!
+
             card2 shouldBeEqualTo card
 
-            // BankAccount 만 삭제 
+            /**
+             * BankAccount 만 삭제
+             * ```sql
+             * DELETE FROM billing WHERE billing.dtype = 'BANK_ACCOUNT'
+             * ```
+             */
             BillingTable.deleteWhere { BillingTable.dtype eq BillingType.BANK_ACCOUNT }
+            /**
+             * ```sql
+             * SELECT COUNT(billing.id) FROM billing WHERE billing.dtype = 'BANK_ACCOUNT'
+             * ```
+             */
             BankAccount.count(BillingTable.dtype eq BillingType.BANK_ACCOUNT) shouldBeEqualTo 0L
+
+            /**
+             * ```sql
+             * SELECT COUNT(billing.id) FROM billing WHERE billing.dtype = 'BANK_ACCOUNT'
+             * ```
+             */
             BankAccount.countBankAccount() shouldBeEqualTo 0L
 
-            // 같은 테이블이지만, bank account의 모든 정보가 삭제되어도, credit card 정보는 남아있다
-            CreditCard.count() shouldBeEqualTo 1L
+            /**
+             * 같은 테이블이지만, bank account의 모든 정보가 삭제되어도, credit card 정보는 남아있다
+             * ```sql
+             * SELECT COUNT(billing.id) FROM billing WHERE billing.dtype = 'CREDIT_CARD'
+             * ```
+             */
+            CreditCard.countCreditCard() shouldBeEqualTo 1L
 
+            /**
+             * ```sql
+             * DELETE FROM billing WHERE billing.dtype = 'CREDIT_CARD'
+             * ```
+             */
             BillingTable.deleteWhere { BillingTable.dtype eq BillingType.CREDIT_CARD }
+
+            /**
+             * ```sql
+             * SELECT COUNT(billing.id) FROM billing WHERE billing.dtype = 'CREDIT_CARD'
+             * ```
+             */
             CreditCard.countCreditCard() shouldBeEqualTo 0L
         }
     }
