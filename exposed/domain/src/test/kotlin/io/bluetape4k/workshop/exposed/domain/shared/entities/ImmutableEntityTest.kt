c38189f2770928a315a8a32169d27e48c6a1eee5
Debop <sunghyouk.bae@gmail.com>
2025-02-26 00:19:16 +0900

    chore: Polishing

--- exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/shared/entities/ImmutableEntityTest.kt
+++ exposed/domain/src/test/kotlin/io/bluetape4k/workshop/exposed/domain/shared/entities/ImmutableEntityTest.kt
@@ -4,6 +4,7 @@ import io.bluetape4k.exposed.dao.idEquals
 import io.bluetape4k.exposed.dao.idHashCode
 import io.bluetape4k.exposed.dao.toStringBuilder
 import io.bluetape4k.logging.KLogging
+import io.bluetape4k.logging.debug
 import io.bluetape4k.workshop.exposed.AbstractExposedTest
 import io.bluetape4k.workshop.exposed.TestDB
 import io.bluetape4k.workshop.exposed.domain.shared.entities.ImmutableEntityTest.Schema.ECachedOrganization
@@ -24,7 +25,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 
 /**
- * 읽기 전용 엔티티 사용 예
+ * 읽기 전용 / 캐싱된 엔티티 사용 예
  */
 class ImmutableEntityTest: AbstractExposedTest() {
 
@@ -33,6 +34,7 @@ class ImmutableEntityTest: AbstractExposedTest() {
     object Schema {
         /**
          * ```sql
+         * -- Postgres
          * CREATE TABLE IF NOT EXISTS organization (
          *      id BIGSERIAL PRIMARY KEY,
          *      "name" VARCHAR(256) NOT NULL,
@@ -46,46 +48,48 @@ class ImmutableEntityTest: AbstractExposedTest() {
         }
 
         /**
-         * 읽기 전용 엔티티 사용 예
+         * 읽기 전용 엔티티
          *
          * 엔티티의 값을 변경하려면 forceUpdateEntity 메서드를 사용해야 한다.
          */
         class EOrganization(id: EntityID<Long>): LongEntity(id) {
-            companion object: ImmutableEntityClass<Long, EOrganization>(Schema.Organization, EOrganization::class.java)
+            // 읽기 전용 엔티티를 사용하기 위해서는 ImmutableEntityClass 를 사용한다.
+            companion object: ImmutableEntityClass<Long, EOrganization>(Organization, EOrganization::class.java)
 
+            // immutable 이므로 모든 프로퍼티는 val 로 선언한다.
             val name: String by Schema.Organization.name
             val etag: Long by Schema.Organization.etag
 
             override fun equals(other: Any?): Boolean = idEquals(other)
             override fun hashCode(): Int = idHashCode()
-            override fun toString(): String =
-                toStringBuilder()
-                    .add("name", name)
-                    .add("etag", etag)
-                    .toString()
+            override fun toString(): String = toStringBuilder()
+                .add("name", name)
+                .add("etag", etag)
+                .toString()
         }
 
         /**
-         * 캐시된 엔티티 사용 예
+         * 캐시된 엔티티
          *
          * 엔티티의 값을 변경하려면 forceUpdateEntity 메서드를 사용해야 한다.
          */
         class ECachedOrganization(id: EntityID<Long>): LongEntity(id) {
+            // 읽기 전용, 캐시된 엔티티를 사용하기 위해서는 ImmutableCachedEntityClass 를 사용한다.
             companion object: ImmutableCachedEntityClass<Long, ECachedOrganization>(
-                Schema.Organization,
+                Organization,
                 ECachedOrganization::class.java
             )
 
+            // Cached Entity 는 캐시된 값을 사용한다. (read-only)
             val name: String by Schema.Organization.name
             val etag: Long by Schema.Organization.etag
 
             override fun equals(other: Any?): Boolean = idEquals(other)
             override fun hashCode(): Int = idHashCode()
-            override fun toString(): String =
-                toStringBuilder()
-                    .add("name", name)
-                    .add("etag", etag)
-                    .toString()
+            override fun toString(): String = toStringBuilder()
+                .add("name", name)
+                .add("etag", etag)
+                .toString()
         }
     }
 
@@ -96,7 +100,9 @@ class ImmutableEntityTest: AbstractExposedTest() {
     @MethodSource(ENABLE_DIALECTS_METHOD)
     fun `immutable entity read after update`(testDB: TestDB) {
         withTables(testDB, Organization) {
+            // Immutable 엔티티만 있으므로, DSL 로 새로운 레코드를 생성합니다.
             transaction {
+                // INSERT INTO organization ("name", etag) VALUES ('JetBrains', 0)
                 Organization.insert {
                     it[name] = "JetBrains"
                     it[etag] = 0
@@ -106,20 +112,12 @@ class ImmutableEntityTest: AbstractExposedTest() {
             transaction {
                 val org = EOrganization.all().single()
 
-                /**
-                 * Immutable 엔티티를 강제로 업데이트
-                 * ```sql
-                 * UPDATE organization SET etag=42 WHERE organization.id = 1
-                 * ```
-                 */
+                // Immutable 엔티티를 강제로 업데이트
+                // UPDATE organization SET etag=42 WHERE organization.id = 1
                 EOrganization.forceUpdateEntity(org, Organization.etag, 42)
 
-                /**
-                 * 강제 업데이트된 정보를 DB로부터 읽어온다.
-                 * ```sql
-                 * SELECT organization.id, organization."name", organization.etag FROM organization
-                 * ```
-                 */
+                // 강제 업데이트된 정보를 DB로부터 읽어온다.
+                // SELECT organization.id, organization."name", organization.etag FROM organization
                 EOrganization.all().single().etag shouldBeEqualTo 42L
             }
         }
@@ -130,12 +128,14 @@ class ImmutableEntityTest: AbstractExposedTest() {
     fun `immutable entity read after update with cached entity`(testDB: TestDB) {
         withTables(testDB, Organization) {
             transaction {
+                // INSERT INTO organization ("name", etag) VALUES ('JetBrains', 0)
                 Organization.insert {
                     it[name] = "JetBrains"
                     it[etag] = 0
                 }
             }
             transaction {
+                // UPDATE organization SET "name"='JetBrains Inc.'
                 Organization.update {
                     it[name] = "JetBrains Inc."
                 }
@@ -144,29 +144,19 @@ class ImmutableEntityTest: AbstractExposedTest() {
             transaction {
                 val org = ECachedOrganization.all().single()
 
-                /**
-                 * Immutable Cached 엔티티를 강제로 업데이트
-                 *
-                 * ```sql
-                 * UPDATE organization SET "name"='JetBrains Gmbh' WHERE organization.id = 1
-                 * ```
-                 */
+                // Immutable Cached 엔티티를 강제로 업데이트
+                //  UPDATE organization SET "name"='JetBrains Gmbh' WHERE organization.id = 1
                 ECachedOrganization.forceUpdateEntity(org, Organization.name, "JetBrains Gmbh")
 
-                /**
-                 * DSL 을 이용하여 직접 업데이트한 경우 Cached 엔티티에 반영되지 않는다.
-                 *
-                 * ```sql
-                 * UPDATE organization SET etag=1 WHERE organization.id = 1
-                 * ```
-                 */
+                // DSL 을 이용하여 직접 업데이트한 경우 Cached 엔티티에 반영되지 않는다.
+                // UPDATE organization SET etag=1 WHERE organization.id = 1
                 Organization.update({ Organization.id eq org.id }) {
                     it[etag] = 1
                 }
+                // 엔티티에는 DSL Update 가 반영 안된다.
                 org.name shouldBeEqualTo "JetBrains Gmbh"
                 org.etag shouldBeEqualTo 0L
 
-                // Populate _cachedValues in ImmutableCachedEntityClass with inconsistent entity value
                 // DB에서 다시 로드 시에는 값이 Update 된다.
                 val org2 = ECachedOrganization.all().single()
 
@@ -177,9 +167,11 @@ class ImmutableEntityTest: AbstractExposedTest() {
                 org.etag shouldNotBeEqualTo org2.etag
             }
 
+            log.debug { "New other transaction" }
+
             // 다른 Transaction 에서는 다른 캐시를 사용하므로 업데이트된 값을 읽어온다.
             transaction {
-                // 모두 캐시되어 있으므로 실제 쿼리를 실행하지 않음
+                // 모두 캐시되어 있으므로 실제 DB로 부터 읽어오지는 않습니다.
                 val org = ECachedOrganization.all().single()
 
                 org.name shouldBeEqualTo "JetBrains Gmbh"
