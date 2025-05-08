29f81ac87f7178fbb811ee50f5ee8e7e43d06561
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-05-09 00:08:34 +0900

    chore: 테스트 코드 개선 (Flow 를 사용하도록 수정)

--- spring-boot/webflux-websocket/src/test/kotlin/io/bluetape4k/workshop/webflux/WebsocketApplicationTest.kt
+++ spring-boot/webflux-websocket/src/test/kotlin/io/bluetape4k/workshop/webflux/WebsocketApplicationTest.kt
@@ -6,14 +6,17 @@ import io.bluetape4k.logging.debug
 import io.bluetape4k.spring.tests.httpGet
 import io.bluetape4k.workshop.webflux.model.Event
 import io.bluetape4k.workshop.webflux.model.Quote
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.reactive.asFlow
+import org.amshove.kluent.shouldBeGreaterThan
 import org.amshove.kluent.shouldBeTrue
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
-import org.springframework.test.web.reactive.server.WebTestClient.ListBodySpec
-import org.springframework.test.web.reactive.server.expectBodyList
+import org.springframework.test.web.reactive.server.returnResult
 import java.math.BigDecimal
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -31,27 +34,25 @@ class WebsocketApplicationTest(
     fun `get quotes`() = runSuspendIO {
         client.httpGet("/quotes")
             .expectHeader().contentType(MediaType.APPLICATION_NDJSON)
-            .expectBodyList<Event>()
-            .consumeWith<ListBodySpec<Event>> { result ->
-                val events = result.responseBody!!
-                events.all { event ->
-                    log.debug { "received event=$event" }
-                    event.data.all { it.price > BigDecimal.ZERO }
-                }.shouldBeTrue()
+            .returnResult<Event>().responseBody
+            .asFlow()
+            .onEach { event ->
+                log.debug { "received event=$event" }
+                event.data.all { it.price > BigDecimal.ZERO }.shouldBeTrue()
             }
+            .collect()
     }
 
     @Test
     fun `fetch quotes by flow`() = runSuspendIO {
-        client.httpGet("/quotes/200")
+        client.httpGet("/quotes/100")
             .expectHeader().contentType(MediaType.APPLICATION_NDJSON)
-            .expectBodyList<Quote>()
-            .consumeWith<ListBodySpec<Quote>> { result ->
-                val quotes = result.responseBody!!
-                quotes.all { quote ->
-                    log.debug { "received quote=$quote" }
-                    quote.price > BigDecimal.ZERO
-                }.shouldBeTrue()
+            .returnResult<Quote>().responseBody
+            .asFlow()
+            .onEach { quote ->
+                log.debug { "received quote=$quote" }
+                quote.price shouldBeGreaterThan BigDecimal.ZERO
             }
+            .collect()
     }
 }
