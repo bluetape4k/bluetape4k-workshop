b7b2dc667624f314125df825069e13d1ceea361a
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-09-22 23:10:00 +0900

    - build: Kotlin 컴파일러 플래그 정리 (-Xenable-builder-inference 주석 처리) - test: unsigned 타입 비교 테스트에서 명시적 타입 캐스팅 추가 (toUByte(), toUShort())

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,7 +95,7 @@ subprojects {
                 "-Xjvm-default=all",
                 "-Xinline-classes",
                 "-Xstring-concat=indy",         // since Kotlin 1.4.20 for JVM 9+
-                "-Xenable-builder-inference",   // since Kotlin 1.6
+                // "-Xenable-builder-inference",   // since Kotlin 1.6
                 "-Xcontext-receivers"           // since Kotlin 1.6
             )
             val experimentalAnnotations = listOf(
