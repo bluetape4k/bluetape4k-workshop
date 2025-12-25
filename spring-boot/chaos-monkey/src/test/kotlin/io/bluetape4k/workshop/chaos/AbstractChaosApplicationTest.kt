6ed811de944c15d4c712cba05f1211f6b1c78690
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-12-25 22:41:30 +0900

    - build: Jackson 3.x 관련 의존성 변경 및 충돌 라이브러리 제거 - build: WebTestClient 설정 수정 및 활용 방식 통합 - refactor: Task 및 Resilience4j 컨트롤러 상속 제거 - refactor: 테스트 코드에서 httpXxx 메서드 제거 및 WebTestClient 체계 수정 - chore: Chaos Monkey 관련 FIXME 추가 (Spring Boot 4 미지원)

--- spring-boot/chaos-monkey/src/test/kotlin/io/bluetape4k/workshop/chaos/AbstractChaosApplicationTest.kt
+++ spring-boot/chaos-monkey/src/test/kotlin/io/bluetape4k/workshop/chaos/AbstractChaosApplicationTest.kt
@@ -1,20 +1,21 @@
 package io.bluetape4k.workshop.chaos
 
 import io.bluetape4k.logging.KLogging
-import io.bluetape4k.support.uninitialized
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.ApplicationContext
+import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 abstract class AbstractChaosApplicationTest {
     companion object: KLogging()
 
-    @Autowired
-    protected val context: ApplicationContext = uninitialized()
+    @LocalServerPort
+    protected var port: Int = 0
 
     protected val client: WebTestClient by lazy {
-        WebTestClient.bindToApplicationContext(context).build()
+        WebTestClient
+            .bindToServer()
+            .baseUrl("http://localhost:$port")
+            .build()
     }
 }
